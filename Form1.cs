6c6fb5cefc2647fd04156855cff7fa2f9760a236
Jorick Caberio <circuitfreakcoder@gmail.com>
2013-03-16 10:12:35 +0800

    C# Practical

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            textBoxOutput.Text  =    textBoxInput.Text.GroupBy(x => x).OrderByDescending(x => x.Count()).First().Key.ToString();
+        }
+    }
+}
